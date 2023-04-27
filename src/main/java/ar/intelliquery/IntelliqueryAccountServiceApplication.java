cfd9fd5c8b60bc336be47dae6de8b8cb15aa0bb4
enzolcastro <enzo.l.castro@gmail.com>
2023-04-26 21:04:41 -0300

    code

---
+++ src/main/java/ar/intelliquery/IntelliqueryAccountServiceApplication.java
@@ -0,0 +1,13 @@
+package ar.intelliquery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntelliqueryAccountServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntelliqueryAccountServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ar/intelliquery/IntelliqueryAccountServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.intelliquery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntelliqueryAccountServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
